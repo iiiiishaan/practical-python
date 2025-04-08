9e3b2b03f269e98e58daa6f11b769ab5574bbaeb
Ishaan <ishaan2april@gmail.com>
2025-04-09 01:19:24 +0530

    completed pcost.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,10 @@
-# mortgage.py
-#
-# Exercise 1.7
+mortgage = 500000
+rate = 0.05
+total_payment = 0.0
+monthly_payment = 2684.11
+
+while mortgage>0:
+    mortgage = mortgage + mortgage*rate/12 - monthly_payment
+    total_payment = total_payment + monthly_payment
+
+print("total:",total_payment) 
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
-# pcost.py
-#
-# Exercise 1.27
+
+total_cost = 0.0
+file = open("/home/ishaan/practical-python/Work/Data/portfolio.csv", "rt")
+headers = next(file).split(',')
+headers
+
+for line in file:
+	row = line.split(",")
+	print(row)
+
+
+	shares = int(row[1])
+	prices = float(row[2])
+
+	total_cost += shares*prices
+
+	print(total_cost)
